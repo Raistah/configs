2bfb552b06889a8b9366d3caae3b770872659fef
raistah <bigkuraz@gmail.com>
2025-08-04 21:08:56 +0300

    add zed, modify hyprland conf

--- home/zed.nix
+++ home/zed.nix
@@ -3,4 +3,13 @@
   programs.zed-editor = {
     enable = true;
   };
+
+  home.file = {
+    ".config/zed/keymap.json" = {
+      source = ./dotfiles/zed-keymap.jsonc;
+    };
+    ".config/zed/settings.json" = {
+      source = ./dotfiles/zed-settings.jsonc;
+    };
+  };
 }
