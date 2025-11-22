e85c97da28ebfb8849429271fef99b51ebd58aca
raistah <bigkuraz@gmail.com>
2025-11-22 19:18:41 +0200

    feat: make waybar run as systemd

--- home/fish.nix
+++ home/fish.nix
@@ -7,12 +7,12 @@
       set fish_greeting # Disable greeting
 
       function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+       	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+       	yazi $argv --cwd-file="$tmp"
+       	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+      		builtin cd -- "$cwd"
+       	end
+       	rm -f -- "$tmp"
       end
     '';
 
--- home/waybar.nix
+++ home/waybar.nix
@@ -2,6 +2,7 @@
 {
   programs.waybar = {
     enable = true;
+    systemd.enable = true;
   };
 
   home.file = {
